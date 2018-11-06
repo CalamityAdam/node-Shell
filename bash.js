d953f59bcbc555577d9101dc7ff61ac710cd39ef
Michael <mchang22@buffalo.edu>
2018-11-06 12:06:24 -0600

    added ls and finished pwd

--- bash.js
+++ bash.js
@@ -1,25 +1,28 @@
-const chalk = require('chalk');
+const pwd = require("./pwd");
+const ls = require("./ls");
+
+
+const chalk = require("chalk");
 
 const returnRandomColor = () => {
-  const colors = ['red', 'cyan', 'magenta', 'green', 'yellow'];
+  const colors = ["red", "cyan", "magenta", "green", "yellow"];
 
   const randomIndex = Math.floor(Math.random() * colors.length);
 
   return colors[randomIndex];
 };
-console.log(chalk[returnRandomColor()]('------------------------------'));
+console.log(chalk[returnRandomColor()]("------------------------------"));
 
-process.stdout.write('prompt > ');
+process.stdout.write("prompt > ");
 
-process.stdin.on('data', data => {
+process.stdin.on("data", data => {
   const cmd = data.toString().trim();
-  if (cmd === 'pwd') {
-    // process.stdout.write(__dirname);
-    process.stdout.write(process.cwd());
+  if (cmd === "pwd") {
+    pwd();
+  } else if (cmd === "ls") {
+    ls();
   } else {
-    process.stdout.write('You typed: ' + cmd);
+    process.stdout.write("You typed: " + cmd);
   }
-  process.stdout.write('\nprompt > ');
+  process.stdout.write("\nprompt > ");
 });
-
-// process.stdout.write('\n');
